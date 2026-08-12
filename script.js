4d9526fa33ee0aa496821aa9f85e38071c7bee05
vstarr-tkh <vstarr@theknowledgehouse.org>
2026-08-12 11:39:28 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function init(){
+    const box=document.getElementById("box");
+    box.textContent="Hello World!!";
+    box.style.border="black solid 5px";
+    box.style.borderRadius="10px";
+    box.style.backgroundColor="orange";
+    box.style.padding="2rem";
+    console.log("hello world!!");
+}
+
+init();
